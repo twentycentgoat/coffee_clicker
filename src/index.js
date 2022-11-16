11b209588462f6c39f7715a06e3ec88c31aacd11
docbrown-git <102162436+docbrown-git@users.noreply.github.com>
2022-11-16 16:46:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -24,7 +24,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
